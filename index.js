8202a48a316c0402a5837a6d66efd7cf8a345bd7
aggreyrc <aggreyronoh@gmail.com>
2024-07-01 01:33:06 +0300

    Looping lab

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+// Code your solutions in this fil
+let thankYouMessage = [];
+function writeCards(guestName, event) {
+    for(let i = 0; i < guestName.length; i++){
+        thankYouMessage.push(`Thank you, ${guestName[i]}, for the wonderful ${event} gift!`);
+    }
+   return thankYouMessage;
+}
+
+function countDown(number){
+    let i = number;
+    while(i >=0){
+        console.log(i);
+        i--;
+    }
+}
